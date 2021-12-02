cde5ef9d7364c3dbd41ab713d14ec241521c9df6
Christopher Marchant <cmarch240@gmail.com>
2021-12-02 13:27:46 -0600

    Fixed bug in create_hash_of_array_elements (works now) and created final method substrings

--- substring.rb
+++ substring.rb
@@ -3,11 +3,14 @@ def downcase_array(array)
 end
 
 def create_array_of_substring_matches(phrase, array)
-  array.reduce { |record_array, substring| record_array + phrase.scan(substring) }
+  array.reduce(Array.new(0)) { |record_array, substring| record_array + phrase.scan(substring) }
 end
 
 def create_hash_of_array_elements(array)
-  array.reduce(Hash.new(0)) { |hash, array_element| hash[array_element] += 1}
+  array.reduce(Hash.new(0)) do |hash, array_element| 
+    hash[array_element] += 1
+    hash
+  end
 end
 
 def substrings(phrase, dictionary)
@@ -15,7 +18,12 @@ def substrings(phrase, dictionary)
   downcase_dictionary = downcase_array(dictionary)
 
   array_of_substring_matches = create_array_of_substring_matches(downcase_phrase, downcase_dictionary)
-
+  array_of_substring_matches
   create_hash_of_array_elements(array_of_substring_matches)
 
 end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+phrase = "below"
+
+puts substrings(phrase, dictionary)
