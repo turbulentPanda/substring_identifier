ed2cc520b8916501daf2bcaf9b7cda5736ccef1e
Christopher Marchant <cmarch240@gmail.com>
2021-12-02 13:20:39 -0600

    Defined create_hash_of_array_elements which is passed an array and returns a hash whose keys are the elements of the array and whose values are the number of times the array elements appear in the array

--- substring.rb
+++ substring.rb
@@ -6,3 +6,16 @@ def create_array_of_substring_matches(phrase, array)
   array.reduce { |record_array, substring| record_array + phrase.scan(substring) }
 end
 
+def create_hash_of_array_elements(array)
+  array.reduce(Hash.new(0)) { |hash, array_element| hash[array_element] += 1}
+end
+
+def substrings(phrase, dictionary)
+  downcase_phrase = phrase.downcase
+  downcase_dictionary = downcase_array(dictionary)
+
+  array_of_substring_matches = create_array_of_substring_matches(downcase_phrase, downcase_dictionary)
+
+  create_hash_of_array_elements(array_of_substring_matches)
+
+end
