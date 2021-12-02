88c0d263edd72ee18259025e769d081a35016a9a
Christopher Marchant <cmarch240@gmail.com>
2021-12-02 13:10:30 -0600

    Defined create_array_of_substring_matches(phrase, array) which iterates through each element of array, determines if the element is a substring of phrase, and returns an array of all matching substrings (with repeats)

--- substring.rb
+++ substring.rb
@@ -2,4 +2,7 @@ def downcase_array(array)
   array.map { |element| element.downcase}
 end
 
-puts downcase_array(['HI THERE', 'Hi There?', 'hi there'])
+def create_array_of_substring_matches(phrase, array)
+  array.reduce { |record_array, substring| record_array + phrase.scan(substring) }
+end
+
